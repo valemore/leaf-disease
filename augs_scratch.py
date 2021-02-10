26ba293d2578cfed87b2e8e568c013c29c45449d
Valerio Morelli <morelli.valerio@gmail.com>
2021-02-10 11:06:42 +0100

    vit mosaic

--- augs_scratch.py
+++ augs_scratch.py
@@ -26,28 +26,35 @@ class DummyModel(nn.Module):
         x = self.fc(x)
         return x
 
-dset_2020 = LeafDataset("data/images", "data/images/labels.csv", transform=None)
+original_transforms = A.Compose([A.Resize(386, 386)])
+dset_2020 = LeafDataset("data/images", "data/images/labels.csv", transform=original_transforms)
 img_idx = np.random.randint(len(dset_2020)) # 13955
 
-train_transforms = A.Compose([
-        A.Resize(386, 386),
-        A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(0, 0.0), rotate_limit=0, p=1.0),
-        # A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
-        # A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=20, val_shift_limit=10, p=1.0),
-        # A.RGBShift(10, 10, 10, p=1.0),
-        A.GaussNoise(p=1.0),
-        # A.Transpose(p=0.5),
-        # A.HorizontalFlip(p=0.5),
-        # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, p=1.0)
-    ])
+transforms = A.Compose([
+    A.Resize(386, 386),
+    # A.OneOf(A.ShiftScaleRotate(0, (0.5), 0)),
+    A.ShiftScaleRotate(shift_limit=0.0, scale_limit=(2.0, 2.0), rotate_limit=0, p=1.0),
+    # A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
+    # A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=20, val_shift_limit=10, p=1.0),
+    # A.RGBShift(10, 10, 10, p=1.0),
+    A.GaussNoise(p=1.0),
+    # A.Transpose(p=0.5),
+    # A.HorizontalFlip(p=0.5),
+    # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, p=1.0)
+])
 
-pre_mosaic_singleton_dset = LeafDataset.from_leaf_dataset(dset_2020, [img_idx], transform=train_transforms)
+original_singleton_dset = LeafDataset.from_leaf_dataset(dset_2020, [img_idx], transform=original_transforms)
+singleton_dset = LeafDataset.from_leaf_dataset(dset_2020, [img_idx], transform=transforms)
 
-original_img, original_img_lab, _ = pre_mosaic_singleton_dset[0]
+img, img_lab, _ = singleton_dset[0]
+original_img, original_label, _ = original_singleton_dset[0]
 
 plt.rcParams["figure.figsize"] = (10, 10)
-plt.imshow(original_img)
+fig, axs =plt.subplots(2, 1)
+axs[0].imshow(original_img)
+axs[1].imshow(img)
 plt.show()
+
 
 iter_train_dset = iter(train_dataloader)
 img, lab, idx = next(iter_train_dset)
--- run_mosaic_Feb10.py
+++ run_mosaic_Feb10.py
@@ -14,8 +14,7 @@ from leaf.model import LeafModel, train_one_epoch, validate_one_epoch
 from leaf.sched import get_warmup_scheduler, LinearLR, fix_optimizer
 from leaf.cutmix import CutMix
 from leaf.cutmix_utils import CutMixCrossEntropyLoss
-from leaf.mosaic import Mosaic
-from leaf.sched import MyAnnealing, reset_initial_lr, fix_optimizer
+from leaf.mosaic import Mosaic, MosaicTransform
 from leaf.utils import seed_everything
 
 from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts, CyclicLR, LambdaLR, CosineAnnealingLR, OneCycleLR
@@ -34,11 +33,11 @@ if __name__ == "__main__":
 
     @dataclass
     class CFG:
-        description: str = "vit-base-mosaic"
-        model_file: str = "vit"
+        description: str = "mosaic 7 grid"
+        model_file: str = "tmp"
         num_classes: int = 5
-        img_size: int = 384
-        arch: str = "vit_base_patch16_384"
+        img_size: int = 380
+        arch: str = "tf_efficientnet_b4_ns"
         loss_fn: str = "CutMixCrossEntropyLoss"
         mosaic_beta: float = 2.0
         mosaic_prob: float = 0.5
@@ -52,32 +51,30 @@ if __name__ == "__main__":
 
     num_workers = 4
 
-    batch_size = 18 if on_gcp else 6
-    val_batch_size = 36 if on_gcp else 12
+    batch_size = 36 if on_gcp else 12
+    val_batch_size = 72 if on_gcp else 24
 
-    debug = True
+    debug = False
     if debug:
         batch_size = int(batch_size / 2)
         val_batch_size = int(batch_size / 2)
 
     log_steps = 50 if on_gcp else 200
 
-    max_lr = 0.01
-    min_lr = 1e-5
-    final_lr = 1e-8
+    min_lr = 1e-3
+    max_lr = 0.05
 
-    momentum = 0.9
     weight_decay = 0.0
 
     grad_norm = None
-    
-    num_epochs = 10
+
+    num_epochs = 7
 
     train_transforms = A.Compose([
         A.Resize(CFG.img_size, CFG.img_size),
         A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=90, p=1.0),
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1.0),
-        A.HueSaturationValue(hue_shift_limit=0.0, sat_shift_limit=20.0, val_shift_limit=10.0, p=1.0),
+        A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=20, val_shift_limit=10, p=1.0),
         A.RGBShift(p=1.0),
         A.Transpose(p=0.5),
         A.HorizontalFlip(p=0.5),
@@ -85,7 +82,7 @@ if __name__ == "__main__":
         ToTensorV2()
     ])
 
-    post_cutmix_transforms = None
+    post_mosaic_transforms = None
 
     val_transforms = A.Compose([
         A.Resize(CFG.img_size, CFG.img_size),
@@ -108,7 +105,7 @@ if __name__ == "__main__":
 
         fold_dset = LeafDataset.from_leaf_dataset(dset_2020, train_idxs, transform=None)
         pre_mosaic_train_dset = UnionDataSet(fold_dset, dset_2019, transform=train_transforms)
-        train_dset = Mosaic(pre_mosaic_train_dset, 5, beta=cfg.mosaic_beta, prob=cfg.mosaic_prob)
+        train_dset = Mosaic(pre_mosaic_train_dset, 5, beta = cfg.mosaic_beta, prob=cfg.mosaic_prob)
         val_dset = LeafDataset.from_leaf_dataset(dset_2020, val_idxs, transform=val_transforms)
 
         train_dataloader = LeafDataLoader(train_dset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -118,15 +115,14 @@ if __name__ == "__main__":
         model_prefix = f"dbg_fold{fold}.{datetime.now().strftime('%b%d_%H-%M-%S')}" if debug is True else model_prefix
         leaf_model = LeafModel(cfg, model_prefix=model_prefix, output_dir=output_dir)
 
-        optimizer = SGD(leaf_model.model.parameters(), lr=max_lr, momentum=momentum, weight_decay=weight_decay)
-        # div_factor = max_lr / min_lr
-        # scheduler = OneCycleLR(optimizer, epochs=num_epochs, steps_per_epoch=len(train_dataloader), max_lr=max_lr, div_factor=div_factor)
-        scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=num_epochs * len(train_dataloader) + 1, eta_min=min_lr)
+        optimizer = SGD(leaf_model.model.parameters(), lr=min_lr, momentum=0.0, weight_decay=weight_decay)
+        div_factor = max_lr / min_lr
+        scheduler = OneCycleLR(optimizer, epochs=num_epochs, steps_per_epoch=len(train_dataloader), max_lr=max_lr)
         leaf_model.update_optimizer_scheduler(optimizer, scheduler)
 
         neptune.init(project_qualified_name='vmorelli/leaf')
         params_dict = {
-            param: eval(param) for param in ["train_transforms", "post_cutmix_transforms", "val_transforms", "batch_size", "num_epochs", "max_lr", "min_lr", "optimizer", "scheduler", "grad_norm"]
+            param: eval(param) for param in ["train_transforms", "post_mosaic_transforms", "val_transforms", "batch_size", "num_epochs", "max_lr", "min_lr", "optimizer", "scheduler", "grad_norm"]
         }
         params_dict.update(cfg.__dict__)
         neptune_tags = []
@@ -148,20 +144,5 @@ if __name__ == "__main__":
             neptune.log_metric("loss/val", y=val_loss, x=steps_offset)
             neptune.log_metric("acc/val", y=val_acc, x=steps_offset)
             leaf_model.save_checkpoint(f"{epoch_name}", epoch_name=f"{epoch_name}", global_step=steps_offset)
-
-        epoch_name = f"{model_prefix}-{epoch}-final"
-        current_lr = leaf_model.optimizer.param_groups[0]["lr"]
-        reset_initial_lr(leaf_model.optimizer)
-        scheduler = MyAnnealing(leaf_model.optimizer, num_steps=len(train_dataloader), start_lr=current_lr, stop_lr=final_lr)
-        leaf_model.update_optimizer_scheduler(leaf_model.optimizer, scheduler)
-
-        train_one_epoch(leaf_model, train_dataloader, log_steps=log_steps, epoch_name=epoch_name, steps_offset=steps_offset, neptune=neptune, grad_norm=grad_norm)
-        steps_offset += len(train_dataloader)
-        val_loss, val_acc = validate_one_epoch(leaf_model, val_dataloader)
-        print(f"Validation after step {steps_offset}: loss {val_loss}, acc {val_acc}")
-        val_step = len(train_dataloader) * epoch
-        neptune.log_metric("loss/val", y=val_loss, x=steps_offset)
-        neptune.log_metric("acc/val", y=val_acc, x=steps_offset)
-        leaf_model.save_checkpoint(f"{epoch_name}", epoch_name=f"{epoch_name}", global_step=steps_offset)
 
         neptune.stop()
